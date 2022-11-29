d8a475fafd132ddc3a86f67d526e7c8ce66e9e60
stafr4300@gmail.com <user@AL01353982.local>
2022-11-29 22:45:33 +0900

    bookstore done

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "jrpark:naver123/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:sde585766!@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	"github/com/gorilla/mux"
+	"github.com/gorilla/mux"
 
-	"./controllers"
 	"github.com/jrpark0704/go-bookstore/pkg/controllers" // "./controllers" same
 )
 
@@ -11,6 +10,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{bookId", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
